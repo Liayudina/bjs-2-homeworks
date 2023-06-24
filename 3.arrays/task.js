91f5b051952a3bb7c39ad9fb1b21ff4cb7bc75ab
Юлия Юдина <yudina.lia31@yandex.ru>
2023-06-24 11:42:26 +0400

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,15 @@
 function compareArrays(arr1, arr2) {
-  
+	let compare = arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]);
+	return compare;
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	let filteredUsers = users.filter(user => user.gender === gender);
+	let usersAge = filteredUsers.map(user => user.age);
+
+	if (!users.length) {
+		return 0;
+	} else if (!gender.includes('женский') && !gender.includes('мужской')) {
+		return 0;
+	} else return usersAge.reduce((summAge, ageValue) => summAge + ageValue, 0) / usersAge.length;
 }
